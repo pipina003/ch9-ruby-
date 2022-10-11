c608eb9436f0ed11d4381916280dbcef7aa779be
pipina003 <113317752+pipina003@users.noreply.github.com>
2022-10-11 02:41:31 +0000

    [Start]演習問題

--- new.rb
+++ new.rb
@@ -13,4 +13,14 @@ car=Car.new
 car.turn("右")
 
 car=Car.new #インスタンス名=クラス名.new
-car.run(5)#呼び出し
+car.run(5)#
+
+#クラスメソッド
+class Car #carという設計図
+ def self.run(distance) #selfはクラス自身という意味
+   puts "車で#{distance}キロ走ります。"
+ end
+end
+
+
+Car.run(10)
